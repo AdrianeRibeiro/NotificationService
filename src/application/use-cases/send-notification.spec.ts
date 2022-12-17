0c1c741bd30fbad0e9a5d6ae6cf32ee0fd080d6d
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-12-17 09:52:16 -0300

    ajustes para criar notificaçao pelo controller

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -3,7 +3,7 @@ import { SendNotification } from './send-notification';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const notificationsRepository = new InMemoryNotificationsRepository()
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
